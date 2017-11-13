9694c019c51bff24a8e07c723617152242066a8f
Filipp Zaripov <philippsbucket@gmail.com>
2017-11-13 15:34:11 +0300

    multimodule

---
+++ springboot-webapp/src/main/java/app/controller/MainController.java
@@ -0,0 +1,19 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.commons.lang3.RandomStringUtils;
+
+@RestController
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(){
+
+
+        return "Hello from Spring boot! \n Here is the randoms String from apache-common-lang3:\t"+RandomStringUtils.random(23);
+
+    }
+
+}
